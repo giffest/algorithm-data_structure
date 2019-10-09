471c39480409c74c22e0955c2aab8b073a979ab3
Dmitry <s.dmitry.s@gmail.com>
2019-10-09 12:56:51 +0300

    init

---
+++ Lesson5/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 9, 2019
+ *      Author: Dmitry Shakhtorin
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char** argv) {
+
+
+	return 0;
+}
